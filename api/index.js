270b881d14a9831d4d7c23bbe99e582925ab3a75
Paolo Ghisotti <paolog.crypto@gmail.com>
2025-06-03 21:54:52 +0200

    Remove token exposure from /api/subscribers

--- api/index.js
+++ api/index.js
@@ -61,7 +61,7 @@ app.get("/api/subscribers", (req, res) => {
       email: s.email,
       confirmed: s.confirmed,
       timestamp: s.timestamp,
-      token: s.token         // <--- ora includiamo anche il token
+      //token: s.token         // <--- ora includiamo anche il token
       
     }))
   });
